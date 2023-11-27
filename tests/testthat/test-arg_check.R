ba39c3804b9d1421ab7171d151da5ce010d39e24
yhan <yushi.han@pasteur.fr>
2023-11-27 17:37:44 +0100

    add testthat for testing

---
+++ tests/testthat/test-arg_check.R
@@ -0,0 +1,22 @@
+test_that("output of fun_check is a list contains 'problem', 'text', and 'object.name'", {
+    vec1 <- -1:3 # vector of integers
+    resultCheck <- fun_check(
+        data = vec1, 
+        class = "integer", 
+        typeof = NULL, 
+        mode = NULL, 
+        length = NULL, 
+        prop = FALSE, 
+        double.as.integer.allowed = FALSE, 
+        options = NULL, 
+        all.options.in.data = FALSE, 
+        na.contain = FALSE, 
+        neg.values = TRUE, 
+        inf.values = TRUE, 
+        print = FALSE, 
+        data.name = NULL, 
+        fun.name = NULL
+    )
+    expected <- list(problem = FALSE, text = "NO PROBLEM DETECTED FOR THE vec1 OBJECT", object.name = "vec1")
+    expect_equal(resultCheck, expected)
+})
