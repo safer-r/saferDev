404e7d042620e06bf03ee0dcd8298bb2c1d9d37b
yhan <yushi.han@pasteur.fr>
2023-11-28 15:28:39 +0100

    update testthat python_pkg_check()

--- tests/testthat/test-python_pkg_check.R
+++ tests/testthat/test-python_pkg_check.R
@@ -1,10 +1,9 @@
-test_that("multiplication works", {
+test_that("test if the python package is in the computer", {
   result <- python_pkg_check(
     req.package = "serpentine", 
     python.exec.path = ".", 
     python.lib.path = ".",
     lib.path = "."
 )
-  expected <- "SOME VARIABLES OF mean ARE ALSO PRESENT IN :\npackage:base: mean\n"
-  expect_error(result, expected)
+  expect_error(object = result, regexp = NULL)
 })
