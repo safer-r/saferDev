d81f2270ecffe63c8b4c07d8b2b182cb02c8807c
yhan <yushi.han@pasteur.fr>
2023-11-30 14:40:14 +0100

    debug enc_check

--- tests/testthat/test-env_check.R
+++ tests/testthat/test-env_check.R
@@ -1,24 +1,9 @@
 test_that("check if the object is in the environment one step above the env_check() environment, and if yes, returns if the same name exists in above environments", {
-    pos <- 2
-    name <- "mean"
     mean <- 2
-    tempo.name <- rev(as.character(unlist(sys.calls())))
-    tempo.frame <- rev(sys.frames())
-    
-    match.list <- vector("list", length = length(tempo.name) + length(search()))
-    ls.names <- c(tempo.name, search()) # names of the functions + names of the search() environments
-    ls.input <- c(tempo.frame, as.list(search())) # environements of the functions + names of the search() environments
-    names(match.list) <- ls.names # 
-    match.list <- match.list[-c(1:(pos + 1))]
-    result3 <- env_check(name = "mean")
-    expected3 <- paste0("SOME VARIABLES ", paste0("OF ", name), " ARE ALSO PRESENT IN :\n", paste0(names(match.list[ ! sapply(match.list, FUN = is.null)]), ": ", sapply(match.list[ ! sapply(match.list, FUN = is.null)], FUN = paste0, collapse = " "), collapse = "\n"), "\n")
-    expect_equal(result3,expected3)
-    
-    
-    result4 <- env_check(
+    result <- env_check(
         pos = 1,
         name = "mean"
     )
-    expected4 <- paste0("SOME VARIABLES ", paste0("OF ", name), " ARE ALSO PRESENT IN :\n", paste0(names(match.list[ ! sapply(match.list, FUN = is.null)]), ": ", sapply(match.list[ ! sapply(match.list, FUN = is.null)], FUN = paste0, collapse = " "), collapse = "\n"), "\n")
-    expect_equal(result4, expected4)
+    expected <- "SOME VARIABLES OF mean ARE ALSO PRESENT IN :\npackage:base: mean\n"
+    expect_equal(result, expected)
 })
