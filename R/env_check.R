8ca4a41b4bd0b3c2583a634a0c3b6ce26c6787e4
hwang <haiding.wang@pasteur.fr>
2024-04-05 15:01:44 +0200

    Add the external.package.name argument in .base_op_check()

--- R/env_check.R
+++ R/env_check.R
@@ -75,7 +75,9 @@ env_check <- function(
     # end function name
     # critical operator checking
     if(safer_check == TRUE){
-        .base_op_check(external.function.name = function.name)
+        .base_op_check(
+            external.function.name = function.name,
+            external.package.name = package.name)
     }
     # end critical operator checking
     # package checking
