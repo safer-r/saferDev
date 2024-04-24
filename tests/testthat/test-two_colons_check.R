e4e97f4a3421e08576d2b624524586bf0973d2d8
hwang <haiding.wang@pasteur.fr>
2024-04-24 10:43:12 +0200

    write examples in test-two_colons_check.R

--- tests/testthat/test-two_colons_check.R
+++ tests/testthat/test-two_colons_check.R
@@ -1,3 +1,29 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("two_colons_check function works correctly", {
+  # Example datasets
+  vec1 <- 3 
+  vec2 <- 4
+  fun1 <- function(
+    vec1,
+    vec2
+  ){
+    output <- base::sum(vec1,vec3)
+    base::return(output)
+  }
+  vec3 <- 3:8
+  mat1 <- matrix(vec3, nrow = 2, ncol = 3, byrow = TRUE)
+  fun2 <- function(mat1){
+    nc <- base::ncol(mat1)
+    means <- numeric(nc)
+    fot (i in 1:nc){
+      means[i] <- mean(mat1[,i])
+    }
+    base::return(means)
+  } 
+
+  # Test cases
+  # Simple examples
+  test_that("handles simple examples", {
+    expect_no_error(two_colons_check(x = fun1))
+    expect_no_error(two_colons_check(x = fun2))
+  })
 })
