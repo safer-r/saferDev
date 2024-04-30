8148efcbe1ad3db816266b80ff76134fa1d6fb59
hwang <haiding.wang@pasteur.fr>
2024-04-30 13:18:48 +0200

    add seealso and author

--- R/arg_check.R
+++ R/arg_check.R
@@ -37,6 +37,10 @@
 #' - Argument "class" with value "vector" means that the object is tested for class(data) returning only "numeric", "integer", "character", "logical", "complex" or "expression". Please, use another value of class (e.g., class = "call" or class = "list") for other types and class of objects
 #'  
 #' - Since R >= 4.0.0, class(matrix()) returns "matrix" "array", and not "matrix" alone as before. However, use argument class = "matrix" to check for matrix object (of class "matrix" "array" in R >= 4.0.0) and use argument class = "array" to check for array object (of class "array" in R >= 4.0.0).
+#' @seealso \code{\link{match.arg}} to check if a given argument value is one of a predefined set of options.
+#' @author Gael Millot <gael.millot@pasteur.fr>
+#' @author Yushi Han <yushi.han2000@gmail.com>
+#' @author Haiding Wang <haiding.wang@etu.u-paris.fr>
 #' @examples
 #' test <- matrix(1:3)
 #' # arg_check(data = test, print = TRUE, class = "vector", mode = "numeric")  # commented because this example returns an error
