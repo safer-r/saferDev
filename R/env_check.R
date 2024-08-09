d016da1af07e0decc011ff64e2504e727c838f78
gmillot <gael.millot@pasteur.fr>
2024-08-09 15:53:26 +0200

    test of saferDev:::.base_op_check in env_check()

--- R/env_check.R
+++ R/env_check.R
@@ -79,7 +79,7 @@ env_check <- function(
     # end function name
     # critical operator checking
     if(safer_check == TRUE){
-        .base_op_check(
+        saferDev:::.base_op_check(
             external.function.name = function.name,
             external.package.name = package.name
         )
