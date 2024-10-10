ad1c2f0166c67e3632f77c786a3d0c11df592581
gmillot <gael.millot@pasteur.fr>
2024-10-10 20:50:21 +0200

    all_args_here() final safer backbone done

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -3,23 +3,23 @@
 #' Check if 1) required functions are present in required packages and 2) required packages are installed locally.
 #' Simplified version of saferDev::is_function_here(), used as internal function for the other functions of the package.
 #' @param fun Character vector of the names of the required functions, preceded by the name of the package they belong to and a double or triple colon. Example: c("ggplot2::geom_point", "grid::gpar").
-#' @param lib.path Character vector specifying the absolute pathways of the directories containing the listed packages in the fun argument, if not in the default directories. If NULL, the function checks only in the base::.libPaths() default R library folders.
+#' @param lib_path Character vector specifying the absolute pathways of the directories containing the listed packages in the fun argument, if not in the default directories. If NULL, the function checks only in the base::.libPaths() default R library folders.
 #' @param external_function_name Name of the function using the .pack_and_function_check() function.
 #' @param external_package_name Name of the package of the function using the .pack_and_function_check() function.
-#' @returns An error message if at least one of the checked packages is missing in lib.path, or if at least one of the checked functions is missing in the required package, nothing otherwise.
+#' @returns An error message if at least one of the checked packages is missing in lib_path, or if at least one of the checked functions is missing in the required package, nothing otherwise.
 #' @author Gael Millot <gael.millot@pasteur.fr>
 #' @author Yushi Han <yushi.han2000@gmail.com>
 #' @author Haiding Wang <wanghaiding442@gmail.com>
 #' @examples
 #' \dontrun{ # Example that shouldn't be run because this is an internal function
-#' .pack_and_function_check(fun = "ggplot2::notgood", lib.path = base::.libPaths(), external_function_name = "F1", external_package_name = "P1") # this example returns an error
-#' .pack_and_function_check(fun = c("ggplot2::geom_point", "grid::gpar"), lib.path = base::.libPaths(), external_function_name = "F1", external_package_name = "P1")
+#' .pack_and_function_check(fun = "ggplot2::notgood", lib_path = base::.libPaths(), external_function_name = "F1", external_package_name = "P1") # this example returns an error
+#' .pack_and_function_check(fun = c("ggplot2::geom_point", "grid::gpar"), lib_path = base::.libPaths(), external_function_name = "F1", external_package_name = "P1")
 #' }
 #' @keywords internal
 #' @rdname internal_function
 .pack_and_function_check <- function(
     fun, 
-    lib.path,
+    lib_path,
     external_function_name,
     external_package_name
 ){
@@ -29,20 +29,20 @@
     # arguments of the .pack_and_function_check() function are not checked, so use carefully inside other functions
     # ARGUMENTS
     # fun: vector of string of the package::function names to check
-    # lib.path: path of the library folder in the system
+    # lib_path: path of the library folder in the system
     # external_function_name: function name
     # external_package_name: package name
     # RETURN
     # An error message or nothing 
     # DEBUGGING
-    # fun = "ggplot2::geom_point" ; lib.path = "C:/Program Files/R/R-4.3.1/library" ; external_function_name = "fun1" ; external_package_name = "1"
-    # fun = "saferDev:::.colons_check_message" ; ; lib.path = "C:/Program Files/R/R-4.3.1/library" ; external_function_name = "fun1" ; external_package_name = "1"
-    # check of lib.path
+    # fun = "ggplot2::geom_point" ; lib_path = "C:/Program Files/R/R-4.3.1/library" ; external_function_name = "fun1" ; external_package_name = "1"
+    # fun = "saferDev:::.colons_check_message" ; ; lib_path = "C:/Program Files/R/R-4.3.1/library" ; external_function_name = "fun1" ; external_package_name = "1"
+    # check of lib_path
     # full check already done in the main function
-    if(base::is.null(lib.path)){
-        lib.path <- base::.libPaths() # base::.libPaths(new = lib.path) # or base::.libPaths(new = c(base::.libPaths(), lib.path))
+    if(base::is.null(lib_path)){
+        lib_path <- base::.libPaths() # base::.libPaths(new = lib_path) # or base::.libPaths(new = c(base::.libPaths(), lib_path))
     }
-    # end check of lib.path
+    # end check of lib_path
     # main code
     tempo.log <- base::grepl(x = fun, pattern = "^[a-zA-Z][a-zA-Z0-9.]*(:{2}[a-zA-Z]|:{3}\\.[a-zA-Z._])[a-zA-Z0-9._]*$")
     # [a-zA-Z][a-zA-Z0-9.]+ means any single alphabet character (package name cannot start by dot or underscore or num), then any alphanum and dots
@@ -54,7 +54,7 @@
     }
     pkg.fun.name.list <- base::strsplit(x = fun, split = ":{2,3}") # package in 1 and function in 2
     pkg.name <- base::sapply(X = pkg.fun.name.list, FUN = function(x){x[1]})
-    pkg.log <- pkg.name %in% base::rownames(utils::installed.packages(lib.loc = lib.path))
+    pkg.log <- pkg.name %in% base::rownames(utils::installed.packages(lib.loc = lib_path))
     if( ! base::all(pkg.log)){
         tempo <- pkg.name[ ! pkg.log]
         tempo.cat <- base::paste0(
@@ -63,9 +63,9 @@
             " OF THE ", external_package_name, " PACKAGE\nREQUIRED PACKAGE", 
             base::ifelse(base::length(tempo) == 1L, base::paste0(":\n", tempo), base::paste0("S:\n", base::paste(tempo, collapse = "\n"))), 
             "\nMUST BE INSTALLED IN", 
-            base::ifelse(base::length(lib.path) == 1L, "", " ONE OF THESE FOLDERS"), 
+            base::ifelse(base::length(lib_path) == 1L, "", " ONE OF THESE FOLDERS"), 
             ":\n", 
-            base::paste(lib.path, collapse = "\n")
+            base::paste(lib_path, collapse = "\n")
         )
         base::stop(base::paste0("\n\n================\n\n", tempo.cat, "\n\n================\n\n"), call. = FALSE) # == in base::stop() to be able to add several messages between ==
     }
@@ -80,9 +80,9 @@
             "MISSING IN THE INSTALLED PACKAGE", 
             base::ifelse(base::length(tempo) == 1L, base::paste0(":\n", tempo), base::paste0("S:\n", base::paste(tempo, collapse = "\n"))),
             "\n\nIN", 
-            base::ifelse(base::length(lib.path) == 1L, "", " ONE OF THESE FOLDERS"), 
+            base::ifelse(base::length(lib_path) == 1L, "", " ONE OF THESE FOLDERS"), 
             ":\n", 
-            base::paste(lib.path, collapse = "\n")
+            base::paste(lib_path, collapse = "\n")
         )
         base::stop(base::paste0("\n\n================\n\n", tempo.cat, "\n\n================\n\n"), call. = FALSE) # == in base::stop() to be able to add several messages between ==
     }
