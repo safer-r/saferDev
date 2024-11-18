78c564412d35ca494f6539ac6b1b6963120b173d
gmillot <gael.millot@pasteur.fr>
2024-11-18 15:26:20 +0100

    example of test-is_function_here.R updated

--- tests/testthat/test-is_function_here.R
+++ tests/testthat/test-is_function_here.R
@@ -3,15 +3,32 @@ test_that("is_function_here()", {
     path <- "blablabla"
     f2 <- "base::sum"
     f3 <- "graphics::par"
-    
+
+
+  # Simple examples
+    result1 <- saferDev::get_message("is_function_here(fun = base::sum)", kind = "error", print.no = TRUE, text = NULL)
+    # warning LINE 13 can be LINE 22
+    expected1 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN is_function_here()\nTHE fun OBJECT MUST BE CLASS vector AND MODE character\n\n================\n\n\n"
+    testthat::expect_equal(result1, expected1)
+
+    result2 <- saferDev::get_message("is_function_here(x = 'mean')", kind = "error", print.no = TRUE, text = NULL)
+    expected2 <- "ERROR MESSAGE REPORTED:\nIn is_function_here(x = \"mean\") : unused argument (x = \"mean\")\n"
+    testthat::expect_equal(result2, expected2)
+
+    testthat::expect_error(is_function_here(fun = "a"))
+    testthat::expect_error(is_function_here(fun = "f2", lib_path = "a"))
+    testthat::expect_error(is_function_here(fun = "f2", safer_check = "a"))
+
+  # sophisticated example
+
     expect_error(object = is_function_here(
     fun = "wrongFct",
-    lib_path = "."
+    lib_path = "." # should be the problem
     ), regexp = NULL)
     
     expect_error(object = is_function_here(
-        fun = f,
-        lib_path = path
+        fun = f, # should be the problem
+        lib_path = path # should be the problem
     ), regexp = NULL)
 
     expect_no_error(is_function_here(
