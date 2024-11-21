39de77079ed3754c50ffe8fd6167abc3fa5c14f1
Tintin2710 <haiding.wang@etu.u-paris.fr>
2024-11-21 12:19:11 +0100

    tests-get_message, complete package::function

--- tests/testthat/test-get_message.R
+++ tests/testthat/test-get_message.R
@@ -2,22 +2,22 @@ test_that("get_message()", {
     char1 <- "wilcox.test(c(1,1,3), c(1, 2, 4), paired = TRUE)" # single character string
     char2 <- "message('ahah')"
     
-    result1 <- get_message(data = char1, header = FALSE)
-    expect_null(result1)
+    result1 <- saferDev::get_message(data = char1, header = FALSE)
+    testthat::expect_null(result1)
     
-    result2 <- get_message(data = char1, print.no = TRUE)
+    result2 <- saferDev::get_message(data = char1, print.no = TRUE)
     expected2 <- "NO ERROR MESSAGE REPORTED"
-    expect_equal(result2,expected2)
+    testthat::expect_equal(result2,expected2)
     
-    result3 <- get_message(data = char2, print.no = TRUE, text = "IN A")
+    result3 <- saferDev::get_message(data = char2, print.no = TRUE, text = "IN A")
     expected3 <- "NO ERROR MESSAGE REPORTED IN A"
-    expect_equal(result3,expected3)
+    testthat::expect_equal(result3,expected3)
 
-    result4 <- get_message(data = char2, kind = "error", print.no = TRUE, text = "IN A")
+    result4 <- saferDev::get_message(data = char2, kind = "error", print.no = TRUE, text = "IN A")
     expected4 <- "NO ERROR MESSAGE REPORTED IN A"
-    expect_equal(result4,expected4)
+    testthat::expect_equal(result4,expected4)
     
-    result5 <- get_message(
+    result5 <- saferDev::get_message(
         data = char1, 
         kind = "warning", 
         header = FALSE,
@@ -27,5 +27,5 @@ test_that("get_message()", {
         safer_check = TRUE
     )
     expected5 <- "simpleWarning in wilcox.test.default(c(1, 1, 3), c(1, 2, 4), paired = TRUE): cannot compute exact p-value with zeroes\n"
-    expect_equal(result5, expected5)
+    testthat::expect_equal(result5, expected5)
 })
