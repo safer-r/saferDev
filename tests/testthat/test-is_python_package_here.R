75576023ac9cbb0f3fffa17d981e936f9509b1ba
Tintin2710 <haiding.wang@etu.u-paris.fr>
2024-11-23 13:18:39 +0100

    is-python_package_here, add examples

--- tests/testthat/test-is_python_package_here.R
+++ tests/testthat/test-is_python_package_here.R
@@ -2,10 +2,17 @@ test_that("is_python_package_here()", {
     lib_path <- "."
     path <- "blablabla"
     req_pkg <- "serpentine"
+    req_pkg2 <- "not_a_real_package"
+
     
     testthat::expect_error(object = is_python_package_here(
     req_package = req_pkg, 
     lib_path = path
+    ), regexp = NULL)
+
+    testthat::expect_error(object = is_python_package_here(
+        req_package = req_pkg2,
+        lib_path = path
     ), regexp = NULL)
     
     testthat::expect_error(object = is_python_package_here(
@@ -15,4 +22,28 @@ test_that("is_python_package_here()", {
         lib_path = lib_path, 
         safer_check = TRUE
     ), regexp = NULL)
+
+    testthat::expect_error(object = is_python_package_here(
+        req_package = "serpentine", 
+        python_exec_path = ".", 
+        python_lib_path = ".", 
+        lib_path = path, 
+        safer_check = TRUE
+    ), regexp = NULL)
+
+    testthat::expect_error(object = is_python_package_here(
+        req_package = req_pkg,
+        python_exec_path = ".",
+        python_lib_path = ".",
+        lib_path = path
+    ), regexp = NULL)
+
+    testthat::expect_error(object = is_python_package_here(
+        req_package = req_pkg2, 
+        python_exec_path = ".", 
+        python_lib_path = ".", 
+        lib_path = lib_path, 
+        safer_check = TRUE
+    ), regexp = NULL)
+
 })
