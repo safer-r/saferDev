af20d368f48ca2ece3a2213ea3874ce08566364a
gmillot <gael.millot@pasteur.fr>
2025-03-07 17:35:17 +0100

    testthat ok for all functions in local

--- tests/testthat/test-is_function_here.R
+++ tests/testthat/test-is_function_here.R
@@ -8,87 +8,89 @@ testthat::test_that("is_function_here()", {
 
 
   # Simple examples
-    result1 <- saferDev::get_message("is_function_here(fun = base::sum)", kind = "error", print.no = TRUE, text = NULL)
+    result1 <- get_message("is_function_here(fun = base::sum)", kind = "error", print.no = TRUE, text = NULL)
     # warning LINE 13 can be LINE 22
-    expected1 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN is_function_here()\nTHE fun OBJECT MUST BE CLASS vector AND MODE character\n\n================\n\n\n"
+    expected1 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR INSIDE saferDev::is_function_here().\n\nTHE fun ARGUMENT MUST BE CLASS vector AND MODE character\n\n================\n\n\n"
+
     testthat::expect_equal(result1, expected1)
 
-    result2 <- saferDev::get_message("is_function_here(x = 'mean')", kind = "error", print.no = TRUE, text = NULL)
+    result2 <- get_message("is_function_here(x = 'mean')", kind = "error", print.no = TRUE, text = NULL)
     expected2 <- "ERROR MESSAGE REPORTED:\nIn is_function_here(x = \"mean\") : unused argument (x = \"mean\")\n"
     testthat::expect_equal(result2, expected2)
 
-    result3 <- saferDev::get_message("is_function_here(fun = 'base::mean')", kind = "error", print.no = TRUE, text = NULL)
+    result3 <- get_message("is_function_here(fun = 'base::mean')", kind = "error", print.no = TRUE, text = NULL)
     expected3 <- "NO ERROR MESSAGE REPORTED"
     testthat::expect_equal(result3, expected3)
 
-    result4 <- saferDev::get_message("is_function_here(fun = 'base::mean', lib_path = 'base')", kind = "error", print.no = TRUE, text = NULL)
+    result4 <- get_message("is_function_here(fun = 'base::mean', lib_path = 'base')", kind = "error", print.no = TRUE, text = NULL)
     expected4 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN is_function_here() OF THE saferDev PACKAGE\nDIRECTORY PATH INDICATED IN THE lib_path ARGUMENT DOES NOT EXISTS:\nbase\n\n================\n\n\n"
 
-    result5 <- saferDev::get_message("is_function_here(fun = 'base::mean', lib_path = NULL, safer_check = TRUE)", kind = "message", print.no = TRUE, text = NULL)
+    result5 <- get_message("is_function_here(fun = 'base::mean', lib_path = NULL, safer_check = TRUE)", kind = "message", print.no = TRUE, text = NULL)
     expected5 <- "NO STANDARD (NON ERROR AND NON WARNING) MESSAGE REPORTED"
     testthat::expect_equal(result5, expected5)
 
-    result6 <- saferDev::get_message("is_function_here(fun = 'base::mean', lib_path = lib_path1, safer_check = FALSE)", kind = "error", print.no = TRUE, text = NULL)
-    expected6 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN is_function_here() OF THE saferDev PACKAGE\nDIRECTORY PATH INDICATED IN THE lib_path ARGUMENT MUST BE A VECTOR OF CHARACTERS:\n1\n\n================\n\n\n"
+    result6 <- get_message("is_function_here(fun = 'base::mean', lib_path = lib_path1, safer_check = FALSE)", kind = "error", print.no = TRUE, text = NULL)
+    expected6 <- "ERROR MESSAGE REPORTED:\nIn list.files(lib) : invalid 'path' argument\n"
     testthat::expect_equal(result6, expected6)
 
-    result7 <- saferDev::get_message("is_function_here(fun = 'base::mean', lib_path = NULL, safer_check = 1)", kind = "error", print.no = TRUE, text = NULL)
-    expected7 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN is_function_here() OF THE saferDev PACKAGE\nsafer_check ARGUMENT MUST BE EITHER TRUE OR FALSE. HER IT IS:\n1\n\n================\n\n\n"
+    result7 <- get_message("is_function_here(fun = 'base::mean', lib_path = NULL, safer_check = 1)", kind = "error", print.no = TRUE, text = NULL)
+    expected7 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN saferDev::is_function_here().\n\nTHE safer_check ARGUMENT VALUE MUST BE A SINGLE LOGICAL VALUE (TRUE OR FALSE ONLY).\nHERE IT IS:\n1\n\n================\n\n\n"
     testthat::expect_equal(result7, expected7)
 
-    result8 <- saferDev::get_message("is_function_here(lib_path = NULL, safer_check = TRUE)", kind = "error", print.no = TRUE, text = NULL)
-    expected8 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN is_function_here() OF THE saferDev PACKAGE\nFOLLOWING ARGUMENT HAS NO DEFAULT VALUE AND REQUIRE ONE:\nfun\n\n================\n\n\n"
+    result8 <- get_message("is_function_here(lib_path = NULL, safer_check = TRUE)", kind = "error", print.no = TRUE, text = NULL)
+    expected8 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN saferDev::is_function_here().\n\nFOLLOWING ARGUMENT HAS NO DEFAULT VALUE AND REQUIRE ONE:\nfun\n\n================\n\n\n"
     testthat::expect_equal(result8, expected8)
 
-    result9 <- saferDev::get_message("is_function_here(fun = 'mean', lib_path = NULL, safer_check = FALSE)", kind = "error", print.no = TRUE, text = NULL)
-    expected9 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN is_function_here() OF THE saferDev PACKAGE\nTHE STRING IN fun ARGUMENT MUST CONTAIN \"::\" OR \":::.\":\nmean\n\n================\n\n\n"
+    result9 <- get_message("is_function_here(fun = 'mean', lib_path = NULL, safer_check = FALSE)", kind = "error", print.no = TRUE, text = NULL)
+    expected9 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN saferDev::is_function_here().\n\nTHE STRING IN fun ARGUMENT MUST CONTAIN \"::\" OR \":::.\":\nmean\n\n================\n\n\n"
     testthat::expect_equal(result9, expected9)
 
-    result10 <- saferDev::get_message("is_function_here(fun = 'base::mean', lib_path = NULL, safer_check = 'FALSE')", kind = "error", print.no = TRUE, text = NULL)#safer_check = 'FALSE'
-    expected10 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN is_function_here() OF THE saferDev PACKAGE\nsafer_check ARGUMENT MUST BE EITHER TRUE OR FALSE. HER IT IS:\nFALSE\n\n================\n\n\n"
+    result10 <- get_message("is_function_here(fun = 'base::mean', lib_path = NULL, safer_check = 'FALSE')", kind = "error", print.no = TRUE, text = NULL)#safer_check = 'FALSE'
+    expected10 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN saferDev::is_function_here().\n\nTHE safer_check ARGUMENT VALUE MUST BE A SINGLE LOGICAL VALUE (TRUE OR FALSE ONLY).\nHERE IT IS:\nFALSE\n\n================\n\n\n"
     testthat::expect_equal(result10, expected10)
 
-    result11 <- saferDev::get_message("is_function_here(fun = base::c('base::mean', NA), lib_path = NULL, safer_check = FALSE)", kind = "error", print.no = TRUE, text = NULL)
-    expected11 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN is_function_here() OF THE saferDev PACKAGE\nTHE STRING IN fun ARGUMENT MUST CONTAIN \"::\" OR \":::.\":\nNA\n\n================\n\n\n"
+    result11 <- get_message("is_function_here(fun = base::c('base::mean', NA), lib_path = NULL, safer_check = FALSE)", kind = "error", print.no = TRUE, text = NULL)
+    expected11 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN saferDev::is_function_here().\n\nTHE STRING IN fun ARGUMENT MUST CONTAIN \"::\" OR \":::.\":\nNA\n\n================\n\n\n"
+
     testthat::expect_equal(result11, expected11)
 
-    result12 <- saferDev::get_message("is_function_here(fun = '', lib_path = NULL, safer_check = FALSE)", kind = "error", print.no = TRUE, text = NULL)
-    expected12 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN is_function_here() OF THE saferDev PACKAGE\nTHIS ARGUMENT\nfun\nCANNOT CONTAIN \"\"\n\n================\n\n\n"
+    result12 <- get_message("is_function_here(fun = '', lib_path = NULL, safer_check = FALSE)", kind = "error", print.no = TRUE, text = NULL)
+    expected12 <- "ERROR MESSAGE REPORTED:\nError : \n\n================\n\nERROR IN saferDev::is_function_here().\n\nTHIS ARGUMENT\nfun\nCANNOT CONTAIN EMPTY STRING \"\".\n\n================\n\n\n"
     testthat::expect_equal(result12, expected12)
 
     
     
     # do not use safer_check = TRUE because test_that() in CI does not like the package presence checking
-    testthat::expect_error(saferDev::is_function_here(fun = "a", safer_check = FALSE))
-    testthat::expect_error(saferDev::is_function_here(fun = f2, lib_path = "a", safer_check = FALSE))
-    testthat::expect_error(saferDev::is_function_here(fun = f2, lib_path = 2, safer_check = FALSE))
-    testthat::expect_error(saferDev::is_function_here(fun = f2, safer_check = "a", safer_check = FALSE))
-    testthat::expect_no_error(saferDev::is_function_here(fun = f2, safer_check = FALSE))
-    testthat::expect_no_error(saferDev::is_function_here(fun = f3, safer_check = FALSE))
-    testthat::expect_error(saferDev::is_function_here(fun = 1, lib_path = NULL, safer_check = FALSE))
-    testthat::expect_error(saferDev::is_function_here(fun = f2, lib_path = null, safer_check = NULL))
+    testthat::expect_error(is_function_here(fun = "a", safer_check = FALSE))
+    testthat::expect_error(is_function_here(fun = f2, lib_path = "a", safer_check = FALSE))
+    testthat::expect_error(is_function_here(fun = f2, lib_path = 2, safer_check = FALSE))
+    testthat::expect_error(is_function_here(fun = f2, safer_check = "a", safer_check = FALSE))
+    testthat::expect_no_error(is_function_here(fun = f2, safer_check = FALSE))
+    testthat::expect_no_error(is_function_here(fun = f3, safer_check = FALSE))
+    testthat::expect_error(is_function_here(fun = 1, lib_path = NULL, safer_check = FALSE))
+    testthat::expect_error(is_function_here(fun = f2, lib_path = null, safer_check = NULL))
 
 
   # sophisticated example
 
-    testthat::expect_error(object = saferDev::is_function_here(
+    testthat::expect_error(object = is_function_here(
     fun = "wrongFct",
     lib_path = ".", # should be the problem
     safer_check = FALSE # do not set to TRUE because test_that() in CI does not like the package presence checking
     ), regexp = NULL)
     
-    testthat::expect_error(object = saferDev::is_function_here(
+    testthat::expect_error(object = is_function_here(
         fun = f, # should be the problem
         lib_path = path, 
         safer_check = FALSE # do not set to TRUE because test_that() in CI does not like the package presence checking
     ), regexp = NULL)
 
-    testthat::expect_no_error(saferDev::is_function_here(
+    testthat::expect_no_error(is_function_here(
         fun = f2,
         lib_path = NULL,
         safer_check = FALSE # do not set to TRUE because test_that() in CI does not like the package presence checking
     ))
-    testthat::expect_error(saferDev::is_function_here(
+    testthat::expect_error(is_function_here(
         fun = f3,
         lib_path = NULL,
         safer_check = 'FALSE' # do not set to TRUE because test_that() in CI does not like the package presence checking
--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -15,112 +15,112 @@ testthat::test_that("report()", {
 
 
 
-    testthat::expect_error(saferDev::report(caca = "a")) # not a correct argument
-    testthat::expect_error(saferDev::report(path = "."))
-    testthat::expect_error(saferDev::report(data = vec1))
-    testthat::expect_error(saferDev::report(data = vec1, path = 1))
-    testthat::expect_error(saferDev::report(data = vec1, path = 'blabla'))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", overwrite = 'blabla'))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", overwrite = 1))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", rownames.kept = 'blabla'))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", rownames.kept = 1))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", vector.cat = 'blabla'))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", vector.cat = 1))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", noquote = 'blabla'))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", noquote = 1))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", sep = 'blabla'))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", sep = FALSE))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", safer_check = "a"))
-    testthat::expect_error(saferDev::report(data = vec1, path = ".", safer_check = 1))
+    testthat::expect_error(report(caca = "a")) # not a correct argument
+    testthat::expect_error(report(path = "."))
+    testthat::expect_error(report(data = vec1))
+    testthat::expect_error(report(data = vec1, path = 1))
+    testthat::expect_error(report(data = vec1, path = 'blabla'))
+    testthat::expect_error(report(data = vec1, path = ".", overwrite = 'blabla'))
+    testthat::expect_error(report(data = vec1, path = ".", overwrite = 1))
+    testthat::expect_error(report(data = vec1, path = ".", rownames.kept = 'blabla'))
+    testthat::expect_error(report(data = vec1, path = ".", rownames.kept = 1))
+    testthat::expect_error(report(data = vec1, path = ".", vector.cat = 'blabla'))
+    testthat::expect_error(report(data = vec1, path = ".", vector.cat = 1))
+    testthat::expect_error(report(data = vec1, path = ".", noquote = 'blabla'))
+    testthat::expect_error(report(data = vec1, path = ".", noquote = 1))
+    testthat::expect_error(report(data = vec1, path = ".", sep = 'blabla'))
+    testthat::expect_error(report(data = vec1, path = ".", sep = FALSE))
+    testthat::expect_error(report(data = vec1, path = ".", safer_check = "a"))
+    testthat::expect_error(report(data = vec1, path = ".", safer_check = 1))
 
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = string1, 
         path = ".",
         vector.cat = FALSE, 
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = string1, 
         path = ".",
         vector.cat = TRUE, 
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = vec1, 
         path = ".",
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = vec2, 
         path = ".",
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = vec3, 
         path = ".",
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = vec3, 
         path = ".", 
         rownames.kept = TRUE,
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = mat1, 
         path = ".",
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = mat1, 
         path = ".", 
         rownames.kept = TRUE,
         noquote = TRUE, 
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = mat1, 
         path = ".", 
         rownames.kept = TRUE,
         noquote = FALSE, 
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = mat2, 
         path = ".", 
         rownames.kept = TRUE,
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = t1, 
         path = ".",
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = t1, 
         path = ".", 
         rownames.kept = TRUE,
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = t2, 
         path = ".",
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = t2, 
         path = ".", 
         rownames.kept = TRUE,
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
         data = l1, 
         path = ".",
         safer_check = FALSE 
         ))
-    testthat::expect_no_error(saferDev::report(
+    testthat::expect_no_error(report(
           data = mat1, 
-          output = "test.txt", 
+          output = "test2.txt", 
           path = ".", 
           overwrite = TRUE, 
           rownames.kept = TRUE, 
