17841e8d96818680789c5025469931062f9fafa2
gmillot <gael.millot@pasteur.fr>
2025-03-20 09:33:49 +0100

    trying to solve the error only present in CI

--- tests/testthat/test-.noclean_functions.R
+++ tests/testthat/test-.noclean_functions.R
@@ -1,8 +1,36 @@
 testthat::test_that(".noclean_functions()", {
 
     ## data argument values
-    source("https://raw.githubusercontent.com/safer-r/saferDev/main/dev/other/test.R")
-    ini1 = utils::capture.output(test)
+    ini1 = c(
+        'function(', 
+        '        text, ', 
+        '        pattern', 
+        '    ){', 
+        '        # AIM', 
+        '        # extract all function names', 
+        '        # ARGUMENTS', 
+        '        # text: vector of strings', 
+        '        # pattern: regex to extract function names', 
+        '        # RETURN', 
+        '        # A list containing the functions names, each compartment being one of the string of the input vector', 
+        '        # DEBUGGING', 
+        '        # text = ini[1] ; pattern = pattern', 
+        '        FUN1 <- function(){}', 
+        '        #### Find all matches, including trailing "(" #', 
+        '        matches <- gregexpr(pattern = base::paste0(pattern, "\\(#"), text = text) # # test', 
+        '        dt <- base::c(2:8)', 
+        '        matched_strings <- regmatches(x = text, m = matches)[[1]]', 
+        '        FUN1()', 
+        '        # Remove trailing "(" from each match #', 
+        '        tempo4 <- a$regmatches(x = text, m = matches)[[1]] ; sum(1:3) ; a$regmatches(x = 1)', 
+        '        tempo5 <- a$count', 
+        '        tempo.cat <- base::paste0("INTERNAL ERROR 4 IN ", function.name, " OF THE ", package.name, " PACKAGE\nLENGTHS OF col1 (", base::length(roc1()), "), col2 (", base::length(roc2), "), AND col3 (", base::length(roc3), "), SHOULD BE EQUAL\n")', 
+        '        result <- sub("\\($##", "", matched_strings) ; range(1:3) # sub("\\($##", "", matched_strings)', 
+        '        tempo.cat <- base::paste0("IAGE\nLENGTHS OF roc00() (", base::ks.test(roc4()), "), and roc0 (", base::length(roc5), ") SHOULL\n")', 
+        '        return(baba) # base::sub("\\($##", "", matched_strings)', 
+        '        base::return(bobo) # a$sub("\\($##", "", matched_strings)', 
+        '    }'
+    )
     int1 <-  c(15, 17)
     int2 <- c(15, 25)
     int3 <- c(15, 25000)
