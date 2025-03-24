00e578d074afc1cd694b626bd5e0ab37da46b1a7
gmillot <gael.millot@pasteur.fr>
2025-03-24 16:37:37 +0100

    test test-get_message.R

--- tests/testthat/test-get_message.R
+++ tests/testthat/test-get_message.R
@@ -215,7 +215,7 @@ testthat::test_that("get_message()", {
     # end text
     # env
     testthat::expect_no_error(get_message(data = str1, kind = "error", header = TRUE, print_no = FALSE, text = NULL, env = NULL, safer_check = TRUE, lib_path = NULL, error_text = ""))
-    testthat::expect_no_error(print(ls(envir = .GlobalEnv)))
+    assign("str1", str1, envir = .GlobalEnv)
     print(ls(envir = .GlobalEnv))
     search()
     # testthat::expect_no_error(get_message(data = str1, kind = "error", header = TRUE, print_no = FALSE, text = NULL, env = .GlobalEnv, safer_check = TRUE, lib_path = NULL, error_text = ""))
